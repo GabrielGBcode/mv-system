6d28714d4a99f3b1994868158484a76d011b644a
GabrielGB <gabrielgb.xyz@gmail.com>
2024-09-27 22:47:19 -0300

    Create home page with no style

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  systems: { name: string; link: string }[] = [
+    {
+      name: 'SoulMV',
+      link: '/soulmv',
+    },
+    {
+      name: 'MVPEP',
+      link: '/mvpep',
+    },
+    {
+      name: 'PortalRH',
+      link: '/portalrh',
+    },
+  ];
+}
