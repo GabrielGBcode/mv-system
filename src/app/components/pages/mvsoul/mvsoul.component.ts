61b851fc586bc8cb6541eb954b40c8859514b348
GabrielGBcode <gabrielgb.xyz@gmail.com>
2024-12-30 12:05:42 -0300

    Adding footer

--- src/app/components/pages/mvsoul/mvsoul.component.ts
+++ src/app/components/pages/mvsoul/mvsoul.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../../../app.component';
+import { MvpepComponent } from '../mvpep/mvpep.component';
 
 @Component({
   selector: 'app-mvsoul',
   standalone: true,
   imports: [],
   templateUrl: './mvsoul.component.html',
-  styleUrl: './mvsoul.component.css'
+  styleUrl: './mvsoul.component.css',
 })
-export class MvsoulComponent {
-
-}
+export class MvsoulComponent {}
