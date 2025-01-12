e4918d0fba003cd334a7afdd9049af1deeb80081
GabrielGBcode <gabrielgb.xyz@gmail.com>
2025-01-12 20:52:37 -0300

    Add JSON file to make personalizable

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import companyName from '../../../assets/config.json';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
+  companyName: string = companyName.companyName;
 }
